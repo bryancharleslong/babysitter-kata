64869bad4bd25c036b158e004c54bd81a38e5f8e
bryancharleslong <bryancharleslong@gmail.com>
2019-01-08 14:59:18 -0500

    added getCharge method

--- src/main/java/babysitter/Babysitter.java
+++ src/main/java/babysitter/Babysitter.java
@@ -3,14 +3,44 @@ package babysitter;
 public class Babysitter {
 
 	private int startTime;
-
-	public Babysitter(int startTime) {
-		this.startTime = startTime;
-	}
+	private int endTime;
+	private int bedTime;
+	int charge;
 
 	public int getStartTime() {
-		// TODO Auto-generated method stub
-		return 5;
+
+		return startTime;
 	}
 
+	public int getEndTime() {
+
+		return endTime;
+	}
+
+	public int getBedTime() {
+
+		return bedTime;
+	}
+
+	public Babysitter(int startTime, int endTime, int bedTime) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.bedTime = bedTime;
+	}
+
+	public int getCharge() {
+		int sleepCharge = 0;
+		int midnightCharge = 0;
+		int awakeCharge = (bedTime - startTime) * 12;
+		if (endTime < 5) {
+			sleepCharge = (12 - bedTime) * 8;
+			midnightCharge = endTime * 16;
+		} else {
+			sleepCharge = (endTime - bedTime) * 8;
+		}
+
+		charge = awakeCharge + sleepCharge + midnightCharge;
+
+		return charge;
+	}
 }
--- src/test/java/babysitter/BabysitterTest.java
+++ src/test/java/babysitter/BabysitterTest.java
@@ -5,10 +5,32 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BabysitterTest {
+	Babysitter underTest = new Babysitter(5,10,9);
 	@Test
 	public void shouldHaveStartTime() {
-		Babysitter underTest = new Babysitter(5);
 		int startTime = underTest.getStartTime();
 		assertEquals(startTime,5);
 	}
+	@Test
+	public void shouldHaveEndTime() {
+		int endTime = underTest.getEndTime();
+		assertEquals(endTime, 10);
+	}
+	@Test
+	public void shouldHaveBedTime() {
+		int bedTime = underTest.getBedTime();
+		assertEquals(bedTime,9);
+	}
+	@Test
+	public void shouldCalculateCharge() {
+		int charge = underTest.getCharge();
+		assertEquals(charge,56);
+	}
+	Babysitter underTest2 = new Babysitter(5,2,10);
+	@Test
+	public void shouldCalculateChargeAfterMidnight() {
+		int charge = underTest2.getCharge();
+		assertEquals(charge,108);
+	}
+	
 }
